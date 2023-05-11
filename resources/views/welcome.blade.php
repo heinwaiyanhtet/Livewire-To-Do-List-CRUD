bb0f33ce1833c7ae2063f7247f7168549736c004
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2023-05-11 12:53:36 +0630

    live wire component set up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,16 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do list App</title>
+    @livewireStyles
+</head>
+<body>
+    
+    <livewire:to-do-lists />
+    @livewireScripts
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <!-- <link rel="preconnect" href="https://fonts.bunny.net"> -->
-        <!-- <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" /> -->
-
-        <!-- Styles -->
-        <style>
-            <livewire:style />
-        </style>
-    </head>
-
-    <body>
-            <livewire:script />
-    </body>
+</body>
 </html>
