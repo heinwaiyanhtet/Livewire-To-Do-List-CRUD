3adf919b996f9716b8271c7a2c1fb0f06b3df400
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2023-05-13 01:11:58 +0630

    create todos in database successfully

--- app/Http/Livewire/ToDoLists.php
+++ app/Http/Livewire/ToDoLists.php
@@ -1,16 +1,49 @@
 <?php
 
 namespace App\Http\Livewire;
-
 use Livewire\Component;
+use App\Models\Todos as Todos;
+
 
 class ToDoLists extends Component
 {
+
+    public $todos;
+
+    protected $rules = [
+        'todos.*.todos' => 'required|not_null|min:1',
+        'todos.*.priority' => 'nullable|in:high,medium,low',
+        'todos.*.cateogry' => 'nullabe',
+        'todos.*.sorting' => 'nullable|integer',
+        'todos.*.message' => 'nullable',
+        'todos.*.duedata' => 'required|data',
+    ];
     
-    public function submitToDo(){
-        
-        // event.prevent
+    public function resetFields(){
+        $this->todos = '';
     }
+
+
+
+    public function storeToDo(){
+        $this->validate();
+        try
+        {
+            Todos::create([
+                'todos' => $this->todos,
+            ]);
+            session()->flash('success','Post Updated Successfully!!');
+            $this->resetFields();
+         }
+
+        catch(Exception $ex)
+        {
+            session()->flash('error','Something goes wrong!');
+        }
+       
+    }
+
+
 
     public function render()
     {
---
+++ database/migrations/2023_05_12_163105_create_todos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->string('todos');
+            $table->enum('priority',['high','medium','low']);
+            $table->string('category')->nullable();
+            $table->integer('sorting')->nullable();
+            $table->string('message')->nullable();
+            $table->timestamp('duedate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('todos');
+    }
+};
